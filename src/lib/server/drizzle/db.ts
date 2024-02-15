869a1ec0a119253cc8a895da6e99d6ed78702141
Pelps12 <oluwapelps@gmail.com>
2024-02-14 22:04:44 -0600

    Remove database js cache

--- src/lib/server/drizzle/db.ts
+++ src/lib/server/drizzle/db.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { drizzle } from 'drizzle-orm/planetscale-serverless';
 import { connect } from '@planetscale/database';
 import { DATABASE_HOST, DATABASE_PASSWORD, DATABASE_USERNAME } from '$env/static/private';
@@ -7,7 +8,11 @@ import schema from './schema';
 const connection = connect({
 	host: DATABASE_HOST,
 	username: DATABASE_USERNAME,
-	password: DATABASE_PASSWORD
+	password: DATABASE_PASSWORD,
+	fetch: (url: string, init: any) => {
+		delete (init as any)['cache']; // Remove cache header
+		return fetch(url, init);
+	}
 });
 
 const db = drizzle(connection, { schema });
