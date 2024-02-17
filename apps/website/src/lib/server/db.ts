a1c4257fe014ddc43660d49cce03b2bd1eaeaa7e
Pelps12 <oluwapelps@gmail.com>
2024-02-17 14:05:53 -0600

    Remove process env on Db creation

--- apps/website/src/lib/server/db.ts
+++ apps/website/src/lib/server/db.ts
@@ -1,4 +1,5 @@
 import { DATABASE_HOST, DATABASE_PASSWORD, DATABASE_USERNAME } from '$env/static/private';
+import { PUBLIC_URL } from '$env/static/public';
 import { connect, drizzle } from '@repo/db';
 import schema from '@repo/db/schema';
 
@@ -7,7 +8,7 @@ const connection = connect({
 	username: DATABASE_USERNAME,
 	password: DATABASE_PASSWORD,
 	fetch: (url: string, init: any) => {
-		if (!process.env.PUBLIC_URL?.includes('localhost')) delete (init as any)['cache']; // Remove cache header
+		if (!PUBLIC_URL?.includes('localhost')) delete (init as any)['cache']; // Remove cache header
 		return fetch(url, init);
 	}
 });
